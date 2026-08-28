71f8a216648e86c8c4832ed797f268a2e66fddd2
SebastinAF <asntheking098@gmail.com>
2026-08-28 22:10:18 +0530

    Initial NutriHeal project

---
+++ src/main/java/com/nutriheal/app/LandingController.java
@@ -0,0 +1,13 @@
+package com.nutriheal.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingController {
+
+    @GetMapping("/")
+    public String landingPage() {
+         return "forward:/landing.html";
+    }
+}
